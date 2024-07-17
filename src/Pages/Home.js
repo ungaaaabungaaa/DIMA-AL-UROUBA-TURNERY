ba527313bb2320aa82d41112ed0cd5cebde63785
Syed Abdul Muqeeth <samx123786@gmail.com>
2024-07-17 13:43:17 +0530

    added somethign else

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,16 +7,43 @@ import Cards from '../Components/cards';
 import { objectives } from '../Data/objectivesData';
 import { departments } from '../Data/departmentsData';
 import DepartmentCards from '../Components/departmentCards';
+import SomethingImage from '../Resources/AboutUs4.jpg'
 
 function Home() {
   return (
     <>
     <Header></Header>
+    <div class="container">
+      <div class="Something-Else-Image">
+        <img className='something_Image' src={SomethingImage}></img>
+      </div>
+      <div class="Something-else-Text">
+        <h1>Something Else | شيء آخر </h1>
+        <p className='something_p'>Reaching the world in the field of industry, turning, and maintenance is not easy, but with patience and diligence we have reached what we aspire to, and for this reason we are trying to preserve the place we have reached, to always maintain our first ranking in our field, and thus we gain the trust of our customers</p>
+        <br></br>
+        <p className='something_p'>الوصول إلى العالمية في مجال الصناعة والخراطة والصيانة ليس بالأمر السهل، ولكن بالصبر والاجتهاد وصلنا إلى ما نطمح إليه، ولهذا السبب نحاول الحفاظ على المكانة التي وصلنا إليها، لنحافظ دائمًا على مكانتنا الأولى التصنيف في مجالنا، وبالتالي نكتسب ثقة عملائنا</p>
+        <div className='highlights'>
+          <div className='highlights_text'>
+            <h1>8K+</h1>
+            <p>Products Shipped</p>
+          </div>
+          <div className='highlights_text'>
+            <h1>6K+</h1>
+            <p>Happy Customer</p>
+          </div>
+          <div className='highlights_text'>
+            <h1>2K+</h1>
+            <p>Staff Support</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div className='departments_layout'>
       <h2>Departments</h2>
       <h3>Precision-engineered components crafted with expertise and <br></br> efficiency in our advanced  Department’s</h3>
       <br></br>
-      <Marquee pauseOnHover pauseOnClick>
+      <Marquee pauseOnHover pauseOnClick >
             {departments.map((department, index) => (
               <DepartmentCards 
                 key={index} 
@@ -39,6 +66,7 @@ function Home() {
           <Cards objective={objectives[4]} />
           <Cards objective={objectives[5]} />
       </Marquee>
+    <br></br>
     </div>
 
     <Footer></Footer>
