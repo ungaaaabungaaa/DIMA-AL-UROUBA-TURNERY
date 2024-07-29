9c216dfc2bd57ff17d939f42daaaffd19d1e053c
Syed Abdul Muqeeth <samx123786@gmail.com>
2024-07-29 11:49:37 +0530

    added shop page

--- src/Components/header.js
+++ src/Components/header.js
@@ -8,11 +8,11 @@ function Header() {
     <>
     <div className='header'>
        <div className='header_title'>
-          <h2 className='header_h1'>DIMA AL UROUBA</h2>
+          <h2  onClick={() => navigate('/')} className='header_h1'>DIMA AL UROUBA</h2>
        </div>
       
         <div className='header_nav'>
-          <h3 className='header_h3' onClick={() => navigate('/')}>Home</h3>
+          <h3 className='header_h3' onClick={() => navigate('/shop')}>Shop</h3>
           <h3 className='header_h3' onClick={() => navigate('/departments')}>Departments</h3>
           <h3 className='header_h3' onClick={() => navigate('/about')}>About</h3>
           <h3 className='header_h3' onClick={() => navigate('/terms')}>Terms</h3>
