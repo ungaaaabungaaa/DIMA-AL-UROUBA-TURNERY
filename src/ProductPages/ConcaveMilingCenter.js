7781a6fe488c7a39c207e9b8c2cc2dd2c67333ed
Syed Abdul Muqeeth <samx123786@gmail.com>
2024-08-23 22:09:23 +0530

    added arabic faqs

--- src/ProductPages/ConcaveMilingCenter.js
+++ src/ProductPages/ConcaveMilingCenter.js
@@ -27,24 +27,24 @@ function ConcaveMilingCenter() {
       rows: 
       [
         {
-          title: "What products do you manufacture?",
-          content: "We manufacture a wide range of metal parts and components. Our seven departments specialize in various aspects of manufacturing, including CNC machining and lathe work. Additionally, we sell a selection of high-quality tools."
+          title: "What products do you manufacture? | ما هي المنتجات التي تصنعونها؟",
+          content: "We manufacture a wide range of metal parts and components. Our seven departments specialize in various aspects of manufacturing, including CNC machining and lathe work. Additionally, we sell a selection of high-quality tools.| نحن نصنع مجموعة واسعة من الأجزاء والمكونات المعدنية. تتخصص أقسامنا السبعة في جوانب مختلفة من التصنيع، بما في ذلك التصنيع باستخدام الحاسب الآلي والعمل على المخرطة. بالإضافة إلى ذلك، نبيع مجموعة مختارة من الأدوات عالية الجودة."
         },
         {
-          title: "How can I place an order ?",
-          content: "You can place an order by contacting our sales team via email or phone"
+          title: "How can I place an order ? | كيف يمكنني تقديم الطلب؟",
+          content: "You can place an order by contacting our sales team via email or phone | يمكنك تقديم الطلب عن طريق التواصل مع فريق المبيعات لدينا عبر البريد الإلكتروني أو الهاتف"
         },
         {
-          title: "Do you offer custom manufacturing services?",
-          content: "Yes, we offer custom manufacturing services to meet your specific needs. Please reach out to us with your requirements, and our team will work with you to develop a tailored solution"
+          title: "Do you offer custom manufacturing services? | هل تقدمون خدمات التصنيع المخصصة؟",
+          content: "Yes, we offer custom manufacturing services to meet your specific needs. Please reach out to us with your requirements, and our team will work with you to develop a tailored solution | نعم، نحن نقدم خدمات التصنيع المخصصة لتلبية احتياجاتك المحددة. يرجى التواصل معنا بمتطلباتك، وسيعمل فريقنا معك لتطوير حل مخصص"
         },
         {
-          title: "How can I get in touch with customer support?",
-          content: "You can contact our customer support team through the contact form on our website, by email, or by calling our support hotline. We are here to assist you with any questions or concerns you may have."
+          title: "How can I get in touch with customer support? | كيف يمكنني التواصل مع خدمة العملاء؟",
+          content: "You can contact our customer support team through the contact form on our website, by email, or by calling our support hotline. We are here to assist you with any questions or concerns you may have. | يمكنك التواصل مع فريق دعم العملاء لدينا من خلال نموذج الاتصال الموجود على موقعنا الإلكتروني، أو عبر البريد الإلكتروني، أو عن طريق الاتصال بخط الدعم الساخن الخاص بنا. نحن هنا لمساعدتك في أي أسئلة أو مخاوف قد تكون لديك"
         },
         {
-          title: "What is your lead time for manufacturing and delivery?",
-          content: "Our lead time for manufacturing varies depending on the complexity of your order and our current production schedule. Typically, it ranges from 1 to 4 weeks. Once your order is processed, we will provide an estimated delivery date"
+          title: "What is your lead time for manufacturing and delivery? | ما هي المهلة الزمنية اللازمة للتصنيع والتسليم؟",
+          content: "Our lead time for manufacturing varies depending on the complexity of your order and our current production schedule. Typically, it ranges from 1 to 4 weeks. Once your order is processed, we will provide an estimated delivery date | يختلف وقت التسليم لدينا حسب تعقيد طلبك وجدول الإنتاج الحالي لدينا. وعادةً ما يتراوح من أسبوع إلى أربعة أسابيع. بمجرد معالجة طلبك، سنزودك بتاريخ تسليم تقديري"
         }
       
       ]
@@ -89,27 +89,28 @@ function ConcaveMilingCenter() {
   
         <div className='Contact_us'>
           <h1>Do You Have Any Questions? <br></br>
-          Get Help From Us</h1>
+          Get Help From Us | هل لديك أي أسئلة؟
+          احصل على المساعدة منا </h1>
           <div className='contact_us_highlight'>
               <div className='highlight'>
               <svg className='margin_right' xmlns="http://www.w3.org/2000/svg" width="1.4rem" height="1.4rem" viewBox="0 0 24 24">
                 <path fill="#695346" d="m8.6 22.5l-1.9-3.2l-3.6-.8l.35-3.7L1 12l2.45-2.8l-.35-3.7l3.6-.8l1.9-3.2L12 2.95l3.4-1.45l1.9 3.2l3.6.8l-.35 3.7L23 12l-2.45 2.8l.35 3.7l-3.6.8l-1.9 3.2l-3.4-1.45zm.85-2.55l2.55-1.1l2.6 1.1l1.4-2.4l2.75-.65l-.25-2.8l1.85-2.1l-1.85-2.15l.25-2.8l-2.75-.6l-1.45-2.4L12 5.15l-2.6-1.1L8 6.45l-2.75.6l.25 2.8L3.65 12l1.85 2.1l-.25 2.85l2.75.6zm1.5-4.4L16.6 9.9l-1.4-1.45l-4.25 4.25l-2.15-2.1L7.4 12z" />
               </svg>
-                <p>Chat live On Whats App</p>
+                <p>Chat live On Whats App | الدردشة المباشرة على تطبيق واتس اب</p>
               </div>
               <br></br>
               <div className='highlight'>
                 <svg className='margin_right' xmlns="http://www.w3.org/2000/svg" width="1.4rem" height="1.4rem" viewBox="0 0 24 24">
                   <path fill="#695346" d="m8.6 22.5l-1.9-3.2l-3.6-.8l.35-3.7L1 12l2.45-2.8l-.35-3.7l3.6-.8l1.9-3.2L12 2.95l3.4-1.45l1.9 3.2l3.6.8l-.35 3.7L23 12l-2.45 2.8l.35 3.7l-3.6.8l-1.9 3.2l-3.4-1.45zm.85-2.55l2.55-1.1l2.6 1.1l1.4-2.4l2.75-.65l-.25-2.8l1.85-2.1l-1.85-2.15l.25-2.8l-2.75-.6l-1.45-2.4L12 5.15l-2.6-1.1L8 6.45l-2.75.6l.25 2.8L3.65 12l1.85 2.1l-.25 2.85l2.75.6zm1.5-4.4L16.6 9.9l-1.4-1.45l-4.25 4.25l-2.15-2.1L7.4 12z" />
                 </svg>
-                <p>Browse our FAQ</p>
+                <p>Browse our FAQ | تصفح الأسئلة الشائعة لدينا</p>
               </div>
           </div>
           <div className='message_box'>
           <input 
               className='contact_message' 
               type="text" 
-              placeholder="Enter your Message" 
+              placeholder="Enter your Message | أدخل رسالتك" 
               value={message}  
               onChange={(e) => setMessage(e.target.value)}
             />
