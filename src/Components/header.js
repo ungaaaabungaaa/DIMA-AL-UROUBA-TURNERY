7ad020801882223b03dca65e45ee0bb9120927b0
Syed Abdul Muqeeth <samx123786@gmail.com>
2024-09-19 15:57:19 +0530

    responsive header client request completed

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import icon from '../Resources/Logo.webp'
+import icon from '../Resources/Logo.webp';
 import '../Styles/Header.css';
 
 function Header() {
@@ -9,27 +9,25 @@ function Header() {
     const url = 'https://maps.app.goo.gl/UxwJvBMQUqZTuicx6';
     window.open(url, '_blank');
   };
+
   return (
     <>
-    <div className='header'>
-       <div className='header_title'>
-         <img className='logo' onClick={() => navigate('/')} src={icon}></img>
-       </div>
-      
-        <div className='header_nav'>
-          <h3 className='header_h3' onClick={() => navigate('/shop')}>Shop</h3>
-          <h3 className='header_h3' onClick={() => navigate('/departments')}>Departments</h3>
-          <h3 className='header_h3' onClick={() => navigate('/about')}>About</h3>
-          <h3 className='header_h3' onClick={() => navigate('/terms')}>Terms</h3>
-        </div>
-
-
-        <div className='contact'>
-          <button onClick={handleContactClick} className='contact_button'>Locate Us</button>
-        </div>
-
-
-    </div>
+      <header className="header">
+        <nav>
+          <div className="logo">
+            <img className="logo" onClick={() => navigate('/')} src={icon} alt="Logo" />
+          </div>
+          <input type="checkbox" id="menu-toggle" />
+          <label htmlFor="menu-toggle" className="menu-icon">&#9776;</label>
+          <ul className="menu">
+            <li><a href="#"><h3 className='header_h3' onClick={() => navigate('/shop')}>Shop</h3></a></li>
+            <li><a href="#"><h3 className='header_h3' onClick={() => navigate('/departments')}>Departments</h3></a></li>
+            <li><a href="#"><h3 className='header_h3' onClick={() => navigate('/about')}>About</h3></a></li>
+            <li><a href="#"><h3 className='header_h3' onClick={() => navigate('/terms')}>Terms</h3></a></li>
+            <li><a href="#"><h3 className='header_h3' onClick={handleContactClick}>Locate Us</h3></a></li>
+          </ul>
+        </nav>
+      </header>
     </>
   );
 }
