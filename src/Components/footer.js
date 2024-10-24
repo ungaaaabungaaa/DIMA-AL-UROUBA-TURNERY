21ed231e9e036d95f1fa0a0623dc162e2a791549
syed abdul muqeeth <syed_abdul_muqeeth@proton.me>
2024-10-24 18:28:05 +0530

    header changes

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -38,7 +38,7 @@ function Footer() {
           <h1 className='footer_h1'>DIMA AL UROUBA TURNERY</h1>
           <h3 className='footer_h3'>Precision Crafting for a Brighter Tomorrow</h3>
           <div className='footer_titles_2'>
-              <h1 className='footer_h1'>ديما العروبة</h1>
+              <h1 className='footer_h1'>مخرطة ديما العروبة</h1>
               <h3 className='footer_h3'>صناعة دقيقة لمستقبل أكثر إشراقا</h3>
           </div>         
       </div>
